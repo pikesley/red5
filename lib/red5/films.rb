b60b34911f7b0b0500a9b76f27b8c0e90f453e59
pikesley <github@orgraphone.org>
2014-12-22 21:15:37 +0000

    Boom

---
+++ lib/red5/films.rb
@@ -0,0 +1,4 @@
+module Red_5
+  class Films < Entities
+  end
+end
--- spec/red5_spec.rb
+++ spec/red5_spec.rb
@@ -2,21 +2,29 @@ require 'spec_helper'
 
 module Red_5
   describe Root do
-    it 'should have vehicles' do
+    it 'has vehicles' do
       root = Root.all
       expect(root.keys).to include 'films'
     end
   end
 
+  describe Films do
+    it 'finds a film' do
+      film = Films.find 1
+      expect(film['title']).to eq 'A New Hope'
+      expect(film['episode_id']).to eq 4
+    end
+  end
+
   describe Planets do
-    it 'should find a planet', :vcr do
+    it 'finds a planet', :vcr do
       planet = Planets.find 2
       expect(planet['name']).to eq 'Alderaan'
     end
   end
 
   describe People do
-    it 'should find a person', :vcr do
+    it 'finds a person', :vcr do
       person = People.find 7
       expect(person['eye_color']).to eq 'blue'
     end
