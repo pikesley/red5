52ecf0bc3f551f136cff61728eebbcbb4a2a4c99
pikesley <github@orgraphone.org>
2014-12-23 11:36:09 +0000

    Contains actual magic

--- lib/red5/people.rb
+++ lib/red5/people.rb
@@ -1,18 +1,11 @@
 module Red_5
   class People < Entities
-    def get_species
-      r = RestClient::Resource.new self['species'][0]
-      z = r.get.body
-      JSON.parse z
-    end
-
     def method_missing method_name, *args
       mname = method_name.to_s
-
       parts = mname.split('_')
 
       if parts[0] == 'get'
-
+        JSON.parse (RestClient::Resource.new self[parts[1]][0]).get.body
       end
     end
   end
